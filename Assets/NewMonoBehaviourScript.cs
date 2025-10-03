5d4c96a41f495608b74f3bfb7a92c379cb35a3b8
ChocoJack <93693473+ChocoJack@users.noreply.github.com>
2025-10-03 14:18:06 -0700

    Changed Debug Message
    Changed debug message from Hello World to Hello Github

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello World");
+        Debug.Log("Hello Github");
     }
 
     // Update is called once per frame
